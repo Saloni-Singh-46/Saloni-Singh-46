74310ca389106e1de39bb6cdcdd0e5b8f9f59d70
Saloni Singh <jiyasinghhr116@gmail.com>
2024-12-18 12:07:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the heading element
+const heading = document.getElementById("heading");
+
+// Add an event listener for the click event
+heading.addEventListener("click", function () {
+    alert("Welcome to our page!");
+});
